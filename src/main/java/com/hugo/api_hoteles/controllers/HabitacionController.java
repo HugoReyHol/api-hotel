22fef04660b4a51acebc41500d1280531e3dc813
Hugo <hugo.reyhol@educa.jcyl.es>
2025-01-13 12:48:15 +0100

    Documentacion para HabitacionController

--- src/main/java/com/hugo/api_hoteles/controllers/HabitacionController.java
+++ src/main/java/com/hugo/api_hoteles/controllers/HabitacionController.java
@@ -2,6 +2,10 @@ package com.hugo.api_hoteles.controllers;
 
 import com.hugo.api_hoteles.entities.Habitacion;
 import com.hugo.api_hoteles.service.HabitacionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/habitacion")
+@Tag(name = "Habitaciones", description = "Lista de habitaciones")
 public class HabitacionController {
     private HabitacionService habitacionService;
 
@@ -18,17 +23,33 @@ public class HabitacionController {
     }
 
     @PutMapping("ocupada/{idHabitacion}")
+    @Operation(summary = "Ocupa una habitacion", description = "Cambia el estado ocupada de una habitacion a True")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cambio realizado exitosamente"),
+            @ApiResponse(responseCode = "400", description = "Solicitud incorrecta"),
+            @ApiResponse(responseCode = "404", description = "No se encontro la habitacion")
+    })
     public ResponseEntity<?> setOcupada(@PathVariable Long idHabitacion) {
         habitacionService.setOcupada(idHabitacion);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/hotel={idHotel}&tamano={tamano}&minPrecio={minPrecio}&maxPrecio={maxPrecio}")
+    @Operation(summary = "Busca habitaciones", description = "Obtiene una lista de habitaciones que cumplan los parametros")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista obtenida exitosamente"),
+            @ApiResponse(responseCode = "400", description = "Solicitud incorrecta")
+    })
     public ResponseEntity<List<Habitacion>> findAllByHotelAndTamanoAndPrecioBetween(@PathVariable Long idHotel, @PathVariable int tamano, @PathVariable float minPrecio, @PathVariable float maxPrecio) {
         return ResponseEntity.ok(habitacionService.findAllByHotelAndTamanoAndPrecioBetween(idHotel, tamano, minPrecio, maxPrecio));
     }
 
     @PostMapping("save")
+    @Operation(summary = "Guarda una nueva habitacion", description = "Guarda una nueva habitacion en la base de datos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Habitacion guardada exitosamente"),
+            @ApiResponse(responseCode = "400", description = "Solicitud incorrecta")
+    })
     public ResponseEntity<?> saveHabitacion (@RequestBody Habitacion habitacion) {
         System.out.println(habitacion);
         habitacionService.saveHabitacion(habitacion);
@@ -36,6 +57,12 @@ public class HabitacionController {
     }
 
     @DeleteMapping("delete/{idHabitacion}")
+    @Operation(summary = "Eliminar una habitacion por ID", description = "Elimina una habitacion por su ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Habitacion eliminada exitosamente"),
+            @ApiResponse(responseCode = "400", description = "Solicitud incorrecta"),
+            @ApiResponse(responseCode = "404", description = "Habitacion no encontrada")
+    })
     public ResponseEntity<?> deleteHabitacion (@PathVariable Long idHabitacion) {
         habitacionService.deleteHabitacion(idHabitacion);
         return new ResponseEntity<>(HttpStatus.CREATED);
